2aa121c48b5d647b52e46023b85a08f7dd6c4d7d
lovanto <rifkylovanto@gmail.com>
2020-02-21 23:40:43 +0700

    first run

---
+++ cdll.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "cdll.h"
+
+using namespace std;
--- cdll.h
+++ cdll.h
@@ -1,6 +1,12 @@
 #ifndef CDL_H_INCLUDED
 #define CDL_H_INCLUDED
 
+#define first(L) L.first
+#define last(L) L.last
+#define prev(P) P->prev
+#define info(P) P->info
+#define next(P) P->next
+
 
 
 #endif // CDL_H_INCLUDED
